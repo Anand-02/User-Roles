a32de033fd38954e27aa7b0f67090fa2747e2683
Anand Kumar Singh <aks858271@gmail.com>
2024-01-07 05:02:07 +0530

    Database Connection Established

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import "dotenv/config";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `Connection Successfull with Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("MongoDB Connection Error: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
